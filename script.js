517d9fc872cd7f50f19ae1ea3ff10088917fab4f
Pavels Limankins <pavels.limankins@gmail.com>
2023-06-05 16:03:26 +0300

    Coding Exercise 4 Task 2

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ fifthTask(); */
 
 // ______Coding Exercise 4 __ Task 1 ___
 
-function firstTask() {
+f/* unction firstTask() {
     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
     const arr = [3, 5, 8, 16, 20, 23, 50];
     const result = [];
@@ -124,4 +124,25 @@ function firstTask() {
     // Не трогаем
     return result;
 }
-firstTask();
+firstTask(); */
+
+// ______Coding Exercise 4 __ Task 2 ___
+
+function secondTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+    // Пишем решение вот тут
+    for (let i=0; i<data.length; i++) {
+        if (typeof(data[i])==='number') {
+            data[i]=data[i]*2;
+        }
+        else if (typeof(data[i])==='string') {
+            data[i]=data[i]+' - done';
+        }
+    }
+    console.log(data);
+    // Не трогаем
+    return data;
+}
+secondTask();
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,15 +1,18 @@
-function firstTask() {
+function secondTask() {
     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const arr = [3, 5, 8, 16, 20, 23, 50];
-    const result = [];
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
 
     // Пишем решение вот тут
-    for (let i=0; i<arr.length; i++) {
-        result[i] = arr[i];
+    for (let i=0; i<data.length; i++) {
+        if (typeof(data[i])==='number') {
+            data[i]=data[i]*2;
+        }
+        else if (typeof(data[i])==='string') {
+            data[i]=data[i]+' - done';
+        }
     }
-    console.log(result);
-    console.log(arr.length);
+    console.log(data);
     // Не трогаем
-    return result;
+    return data;
 }
-firstTask();
+secondTask();
