5fe7a732e261416871cf54cff23dc516e0806ec1
Pavels Limankins <pavels.limankins@gmail.com>
2023-06-14 16:48:47 +0300

    Practice 2

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// ______Practice 1 _______
+
 /* Задание на урок:
 
 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
@@ -180,6 +182,56 @@ let result = '';
     }
 console.log(result); */
 
+
+// ______Practice 2 _______
+
+/* Задание на урок:
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как 
+str.length - и получить её длину)
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+const prompt = require('prompt-sync')();
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
+console.log(personalMovieDB);
+
 // ______Coding Exercise 6____Task 1 
 
 /* function sayHello(name) {
@@ -203,7 +255,7 @@ console.log (a); */
 
 // ______Coding Exercise 6____Task 3
 
-function getMathResult(a, b) {
+/* function getMathResult(a, b) {
     let result=a;
     if (b>0 && typeof(b) === 'number') {
         for (let i=2; i<b+1; i++) {
@@ -213,7 +265,8 @@ function getMathResult(a, b) {
     return result;
 }
 let c = getMathResult (2, 10);
-console.log (c);
+console.log (c); */
+
 
 
 
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,12 +1,36 @@
-function getMathResult(a, b) {
-    let result=a;
-    if (b>0 && typeof(b) === 'number') {
-        for (let i=2; i<b+1; i++) {
-            result += "---"+a*i;
-        }
 
+const prompt = require('prompt-sync')();
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
     }
-    return result;
 }
-let c = getMathResult (2, 10);
-console.log (c);
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
+console.log(personalMovieDB);
